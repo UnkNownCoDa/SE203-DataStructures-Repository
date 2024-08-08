601d26e02092ecfca7239ec9d1cd450cd5e404f3
unknown <guitethangkhanmuan@gmail.com>
2024-08-08 12:04:21 +0530

    Added Menu System

--- Personal/LinkedList.cpp
+++ Personal/LinkedList.cpp
@@ -13,19 +13,23 @@ public:
 class LinkedList{
 public:
     Node *head;
+    LinkedList(void){
+        head = NULL;
+    }
+//Print function
     void print(){
         Node *ptr;
         if (head == NULL){
-            std::cout<<"Empty List\n";
+            std::cout<<"\n\nList: Empty\n\n";
             return;
         }
         ptr = head;
-        std::cout<<"List: ";
+        std::cout<<"\n\nList: ";
         while (ptr->next != NULL){
             std::cout<<ptr->data<<"->";
             ptr = ptr->next;
         }
-        std::cout<<ptr->data<<"->END\n";
+        std::cout<<ptr->data<<"->END\n\n";
     }
 //Insert functions
     void insertEnd(int data){
@@ -102,7 +106,7 @@ public:
         }
         for (ptr = head; ptr->next != NULL; ptr = ptr->next){
             if (ptr->data == data){
-                std::cout<<"Element is in the list";
+                std::cout<<"Element is in the list\n";
                 return;
             }
         }
@@ -110,33 +114,99 @@ public:
             std::cout<<"Element is in the list\n";
             return;
         }
-        std::cout<<"Element not found!\n";
+        std::cout<<"Element is not in the list!\n";
         return;
     }
 };
 
+void MainMenu(void);
+
 int main(void){
     LinkedList list;
-    int data;
-    list.head = NULL;
-    list.print();
-    for (int i = 0; i < 10; ++i){
-        list.insertBegining(i);
+    int data, choice, run = 1;
+    while (run){
+        MainMenu();
+        std::cout<<"Enter you choice: ";
+        std::cin>>choice;
+        switch (choice){
+            case 1:
+                list.print();
+                break;
+            case 2:
+                std::cout<<"Enter enter data: ";
+                std::cin>>data;
+                std::cout<<"Where do you want to insert data:\n";
+                std::cout<<"1. At the beginning\n";
+                std::cout<<"2. At the end\n";
+                std::cout<<"Enter choice: ";
+                std::cin>>choice;
+                switch (choice){
+                    case 1:
+                        list.insertBegining(data);
+                        list.print();
+                        break;
+                    case 2:
+                        list.insertEnd(data);
+                        list.print();
+                        break;
+                    default:
+                        std::cout<<"Invalid input!\n";
+                        run = 0;
+                        break;
+                }
+                break;
+            case 3:
+                std::cout<<"What do you want to delete:\n";
+                std::cout<<"1. Delete beginning\n";
+                std::cout<<"2. Delete end\n";
+                std::cout<<"3. Delete element\n";
+                std::cout<<"Enter choice: ";
+                std::cin>>choice;
+                switch (choice){
+                    case 1:
+                        list.deleteBeginning();
+                        list.print();
+                        break;
+                    case 2:
+                        list.deleteEnd();
+                        list.print();
+                        break;
+                    case 3:
+                        std::cout<<"Enter data: ";
+                        std::cin>>data;
+                        list.deleteElement(data);
+                        list.print();
+                        break;
+                    default:
+                        std::cout<<"Invalid input!\n";
+                        run = 0;
+                        break;
+                }
+                break;
+            case 4:
+                std::cout<<"Enter element to search: ";
+                std::cin>>data;
+                list.print();
+                list.searchElement(data);
+                break;
+            case 5:
+                --run;
+                break;
+            default:
+                std::cout<<"Invalid input!\n";
+                break;
+        }
     }
-    list.print();
-    list.deleteEnd();
-    list.print();
-    list.insertBegining(100);
-    list.print();
-    list.deleteBeginning();
-    list.print();
-    std::cout<<"Enter data to delete: ";
-    std::cin>>data;
-    list.deleteElement(data);
-    list.print();
-    std::cout<<"Enter data to serch: ";
-    std::cin>>data;
-    list.searchElement(data);
-    list.print();
     return 0;
+}
+
+void MainMenu(void){
+    std::cout<<"\n\nWorking with linked lists!!!\n\n";
+    std::cout<<"What do you want to do today:\n";
+    std::cout<<"1. Print list\n";
+    std::cout<<"2. Insert into list\n";
+    std::cout<<"3. Delete from list\n";
+    std::cout<<"4. Search element in list\n";
+    std::cout<<"5. Exit\n";
+
 }
