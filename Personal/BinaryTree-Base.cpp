884d2421f860937efe9008a12034b3db97a25363
unknown <guitethangkhanmuan@gmail.com>
2024-08-09 15:52:29 +0530

    binary trees and renaming of files

---
+++ Personal/BinaryTree-Base.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+
+class Node{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(){
+        this->left = NULL;
+        this->right = NULL;
+    }
+    Node(int data){
+        this ->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+class Tree{
+public:
+    Node *root;
+
+    Tree(){
+        this->root = NULL;
+    }
+
+    Tree(int data){
+        this->root = new Node(data);
+    }
+
+    int isFree(Node *node){
+        return node == NULL;
+    }
+    int isFull(Node *node){
+        return node->left != NULL && node->right == NULL;
+    }
+    void AddNode(int data){
+        Node *node = this->root;
+        if (node == NULL){
+            this->root = new Node(data);
+            return;
+        }
+        while (node != NULL){
+            if (node->data > data){
+                if (isFree(node->left)){
+                    node->left = new Node(data);
+                    return;
+                }
+                node = node->left;
+            }
+            else if (node->data <= data){
+                if (isFree(node->right)){
+                    node->right = new Node(data);
+                    return;
+                }
+                node = node->right;
+            }
+        }
+    }
+    int hasChildren(Node *parent){
+        return parent->left != NULL || parent->right != NULL;
+    }
+    void printNode(Node *parent){
+        if (!hasChildren(parent)){
+            std::cout<<"Parent: "<<parent->data<<" Children: NONE\n";
+            return;
+        }
+
+        std::cout<<"Parent: "<<parent->data<<" Children: ";
+        if (parent->left != NULL){
+            std::cout<< parent->left->data << " ";
+        }
+        if (parent->right != NULL){
+            std::cout<<parent->right->data;
+        }
+        std::cout<<"\n";
+        if (parent->left != NULL){
+            printNode(parent->left);
+        }
+        if (parent->right != NULL){
+            printNode(parent->right);
+        }
+    }
+};
+
+int sum(Node *tree){
+    if (tree->left == NULL && tree->right == NULL){
+        return tree->data;
+    }
+    return tree->data + sum(tree->left) + sum(tree->right);
+}
+
+int convertInt(char *num){
+    int result = 0;
+    for (int i = 0;num[i] != '\0'; ++i){
+        result *= 10;
+        result += num[i] - '0';
+    }
+    return result;
+}
+
+int main(int argc, char** argv){
+    if (argc != 2){
+        std::cout<<"run Command of type: ./a <number>";
+        return 1;
+    }
+    int total;
+    Tree *binary = new Tree();
+    binary->AddNode(4);
+    binary->AddNode(3);
+    binary->AddNode(2);
+    binary->AddNode(8);
+    binary->AddNode(9);
+    binary->AddNode(7);
+    std::cout<<"Printing Node: \n";
+    binary->printNode(binary->root);
+
+    return 0;
+}
